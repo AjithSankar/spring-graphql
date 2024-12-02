aad3519088558681853637b8bcf96a258cda43e0
AjithSankar <mailajithkumars@gmail.com>
2024-12-02 14:57:56 +0530

    Mutation - Update PersonEntity by ID

--- src/main/java/dev/ak/graphql/controller/PersonController.java
+++ src/main/java/dev/ak/graphql/controller/PersonController.java
@@ -5,10 +5,12 @@ import dev.ak.graphql.repository.PersonRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PersonController {
@@ -28,5 +30,19 @@ public class PersonController {
     public Page<Person> allPeoplePaged(@Argument int page, @Argument int size) {
         PageRequest pageRequest = PageRequest.of(page, size);
         return personRepository.findAll(pageRequest);
+    }
+
+    @MutationMapping(value = "updatePerson")
+    public Person update(@Argument int id, @Argument String phoneNumber) {
+        Person person = null;
+        Optional<Person> optionalPerson = personRepository.findById(id);
+        if (optionalPerson.isPresent()) {
+            person = optionalPerson.get();
+            person.setPhoneNumber(phoneNumber);
+        }
+        if (person != null) {
+            personRepository.save(person);
+        }
+        return person;
     }
 }
